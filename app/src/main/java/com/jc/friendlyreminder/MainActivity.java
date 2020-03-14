bf4093407aca6c74b3bb58e30d42e356e724009c
Christine <61249806+CSeeberger@users.noreply.github.com>
2020-03-14 21:59:54 +0100

    i love you

--- app/src/main/java/com/jc/friendlyreminder/MainActivity.java
+++ app/src/main/java/com/jc/friendlyreminder/MainActivity.java
@@ -3,6 +3,8 @@ package com.jc.friendlyreminder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    boolean isVisible = true;
+    public void onClick(View view) {
+
+        Button b = (Button) view;
+        b.setText("");
+
+        if (isVisible) {
+            b.setVisibility(View.INVISIBLE);
+            isVisible = false;
+        } else {
+            b.setVisibility(View.VISIBLE);
+            isVisible = true;
+        }
     }
 }
